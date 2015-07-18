e00886be977a99431bea6478405fe597e428be26
majie <351470428@qq.com>
2015-07-18 17:56:09 +0800

    changping

--- slidingmenu_demo/src/com/online/market/utils/DateUtil.java
+++ slidingmenu_demo/src/com/online/market/utils/DateUtil.java
@@ -4,17 +4,23 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import android.util.Log;
-
 public class DateUtil {
 	
-	public static String getDate(String time){
+	public static String getDate(String str){
 		SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		try {
-			Date date=format.parse(time);
-			Log.d("majie", ""+date.getHours());
+			Date date=format.parse(str);
+//			Log.d("majie", ""+date.getHours());
 			date.setTime(date.getTime()+30*60*1000);
-			return format.format(date);
+			Date currentDate=new Date();
+			long currentTime=currentDate.getTime();
+			long time=date.getTime()-currentTime;
+			if(time<0){
+				return null;
+			}
+			date.setTime(time);
+			SimpleDateFormat f=new SimpleDateFormat("mm分ss秒");
+			return f.format(date);
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
